0a39ce3101b4b4bb18d578e4758b1dbc3983dd47
dreik84 <dreik84@mail.ru>
2026-01-10 10:11:26 +0300

    Update Film.java

--- src/main/java/com/example/film/model/Film.java
+++ src/main/java/com/example/film/model/Film.java
@@ -2,11 +2,14 @@ package com.example.film.model;
 
 import lombok.Data;
 
+import java.time.Duration;
+import java.time.LocalDate;
+
 @Data
 public class Film {
     private long id;
     private String name;
     private String description;
-    private String releaseDate;
-    private int duration;
+    private LocalDate releaseDate;
+    private Duration duration;
 }
