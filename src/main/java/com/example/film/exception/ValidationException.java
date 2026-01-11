ffd46e7f268bcdc05f14ba90ab4753ea16bd53e9
dreik84 <dreik84@mail.ru>
2026-01-11 10:30:11 +0300

    Update ValidationException.java

--- src/main/java/com/example/film/exception/ValidationException.java
+++ src/main/java/com/example/film/exception/ValidationException.java
@@ -1,4 +1,7 @@
 package com.example.film.exception;
 
 public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
 }
