46d9c60c4fc4a7f8026efef5376a9d29b0579204
dreik84 <dreik84@mail.ru>
2026-01-11 10:30:25 +0300

    Update FilmController.java

--- src/main/java/com/example/film/controller/FilmController.java
+++ src/main/java/com/example/film/controller/FilmController.java
@@ -14,10 +14,14 @@ public class FilmController {
 
     @PostMapping("/films")
     public Film addFilm(@RequestBody Film film) {
-        if (film.getName().isBlank()) throw new ValidationException();
-        if (film.getDescription().length() > 200) throw new ValidationException();
-        if (LocalDate.of(1895, 12, 28).isBefore(film.getReleaseDate())) throw new ValidationException();
-        if (film.getDuration().toMillis() <= 0) throw new ValidationException();
+        if (film.getName().isBlank())
+            throw new ValidationException("Название фильма не может быть пустым");
+        if (film.getDescription().length() > 200)
+            throw new ValidationException("Максимальная длина описания - 200 симыолов");
+        if (LocalDate.of(1895, 12, 28).isBefore(film.getReleaseDate()))
+            throw new ValidationException("Дата релиза должна быть не раньше 28 декабря 1895 года");
+        if (film.getDuration().toMillis() <= 0)
+            throw new ValidationException("Продолжительность фильма должна быть положительной");
 
         films.put(film.getId(), film);
         return film;
