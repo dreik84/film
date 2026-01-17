cc47fcb9ed2b0d3193c1011636d5d132d68f5f98
dreik84 <dreik84@mail.ru>
2026-01-17 10:17:58 +0300

    Update InMemory storage

--- src/main/java/com/example/film/storage/InMemoryFilmStorage.java
+++ src/main/java/com/example/film/storage/InMemoryFilmStorage.java
@@ -3,23 +3,27 @@ package com.example.film.storage;
 import com.example.film.model.Film;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Long, Film> films = new HashMap<>();
 
     @Override
     public Film addFilm(Film film) {
-        return null;
+        films.put(film.getId(), film);
+        return film;
     }
 
     @Override
     public Film updateFilm(Film film) {
+        films.put(film.getId(), film);
         return null;
     }
 
     @Override
     public Map<Long, Film> getFilms() {
-        return Map.of();
+        return films;
     }
 }
--- src/main/java/com/example/film/storage/InMemoryUserStorage.java
+++ src/main/java/com/example/film/storage/InMemoryUserStorage.java
@@ -3,23 +3,27 @@ package com.example.film.storage;
 import com.example.film.model.User;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class InMemoryUserStorage implements UserStorage {
+    private final Map<Long, User> users = new HashMap<>();
 
     @Override
     public User addUser(User user) {
-        return null;
+        users.put(user.getId(), user);
+        return user;
     }
 
     @Override
     public User updateUser(User user) {
-        return null;
+        users.put(user.getId(), user);
+        return user;
     }
 
     @Override
     public Map<Long, User> getUsers() {
-        return Map.of();
+        return users;
     }
 }
