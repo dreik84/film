9ac765ee0e8660e017f770e43e4a8c17d29ee9e4
dreik84 <dreik84@mail.ru>
2026-01-19 10:15:04 +0300

    Update UserService

--- src/main/java/com/example/film/service/UserService.java
+++ src/main/java/com/example/film/service/UserService.java
@@ -1,7 +1,30 @@
 package com.example.film.service;
 
+import com.example.film.model.User;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserService {
+    private final Set<Long> friends;
+
+    public UserService() {
+        friends = new HashSet<>();
+    }
+
+    public User addToFriends(User user) {
+        friends.add(user.getId());
+        return user;
+    }
+
+    public User deleteFromFriends(User user) {
+        friends.remove(user.getId());
+        return user;
+    }
+
+    public Set<Long> getFriends() {
+        return friends;
+    }
 }
