b79b64f65465271cd45fbe4c70f6142ab7d48e58
dreik84 <dreik84@mail.ru>
2026-01-20 10:15:33 +0300

    Update FilmService

--- src/main/java/com/example/film/service/FilmService.java
+++ src/main/java/com/example/film/service/FilmService.java
@@ -1,7 +1,30 @@
 package com.example.film.service;
 
+import com.example.film.model.Film;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class FilmService {
+    private final Set<Long> films;
+
+    public FilmService() {
+        films = new HashSet<>();
+    }
+
+    public Film addLike(Film film) {
+        films.add(film.getId());
+        return film;
+    }
+
+    public Film removeLike(Film film) {
+        films.remove(film.getId());
+        return film;
+    }
+
+    public Set<Long> getTopTen() {
+        return films;
+    }
 }
