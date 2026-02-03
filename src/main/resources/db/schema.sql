78710d9eaa7d77be8cb978abf9ae04e549b42529
dreik84 <dreik84@mail.ru>
2026-02-03 10:28:40 +0300

    Create schema.sql

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -10,14 +10,14 @@ CREATE TABLE IF NOT EXISTS films
     rate_mpa     VARCHAR(256),
     release_date DATE,
     duration     INT
-    );
+);
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(256) UNIQUE,
-    login VARCHAR(256) NOT NULL,
-    name VARCHAR(256),
-    birthday DATE,
+    id           BIGINT PRIMARY KEY AUTO_INCREMENT,
+    email        VARCHAR(256) UNIQUE,
+    login        VARCHAR(256) NOT NULL,
+    name         VARCHAR(256),
+    birthday     DATE,
     friend_state VARCHAR(256)
-    );
+);
