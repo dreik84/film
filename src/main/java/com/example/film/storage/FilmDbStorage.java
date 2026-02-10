f6ac2b7fa576f11f30f425a386b225b12e660e5a
dreik84 <dreik84@mail.ru>
2026-02-10 10:35:13 +0300

    Update FilmDbStorage

--- src/main/java/com/example/film/storage/FilmDbStorage.java
+++ src/main/java/com/example/film/storage/FilmDbStorage.java
@@ -1,16 +1,22 @@
 package com.example.film.storage;
 
 import com.example.film.model.Film;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Map;
 import java.util.Optional;
 
 @Qualifier
 @Component
 public class FilmDbStorage implements FilmStorage {
+    private final Logger log = LoggerFactory.getLogger(FilmDbStorage.class);
+
     private final JdbcTemplate jdbcTemplate;
 
     public FilmDbStorage(JdbcTemplate jdbcTemplate) {
@@ -34,6 +40,21 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Optional<Film> getFilmById(Long id) {
+        SqlRowSet filmRow = jdbcTemplate.queryForRowSet("SELECT * films WHERE id = ?", id);
+        if (filmRow.next()) {
+            log.info("Найден фильм: {} {}", filmRow.getLong("id"), filmRow.getString("name"));
+
+            Film film = new Film();
+            film.setId(id);
+            film.setDescription(filmRow.getString("description"));
+            film.setGenre(filmRow.getString("genre"));
+            film.setRateMPA(filmRow.getString("rate_mpa"));
+            film.setReleaseDate(filmRow.getDate("release_date").toLocalDate());
+            film.setDuration(Duration.ofDays(filmRow.getInt("duration")));
+
+            return Optional.of(film);
+        }
+
         return Optional.empty();
     }
 
